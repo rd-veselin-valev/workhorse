fc76d8b6122417fef602fc21462f169c31cea8d9
Veselin-Valev-Rewe <veselin.valev@rewe-group.com>
2025-08-06 09:41:10 +0300

    initial commit

---
+++ src/main/java/com/example/workhorse/WorkhorseApplication.java
@@ -0,0 +1,13 @@
+package com.example.workhorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkhorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkhorseApplication.class, args);
+	}
+
+}
